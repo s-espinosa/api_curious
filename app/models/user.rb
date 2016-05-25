92935f92ebbdbc1884e07cfce15b0b65d5354c2a
Sal Espinosa <sespinos@gmail.com>
2016-05-25 14:41:07 -0600

    Add specific followers and following attributes to show

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,20 @@ class User <OpenStruct
   def self.find(token)
     User.new(service.user_hash(token))
   end
+
+  def service
+    SoundcloudService.new
+  end
+
+  def followers(token)
+    service.find_followers(self.id, token)
+  end
+
+  def following(token)
+    service.find_following(self.id, token)
+  end
+
+  def bigger(avatar)
+    avatar.gsub("large", "t500x500")
+  end
 end
