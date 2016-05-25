52984bfbacea141cb5a24395363f837988a6b9bd
Sal Espinosa <sespinos@gmail.com>
2016-05-25 06:47:23 -0600

    Move user to cookies and refactor

---
+++ app/services/soundcloud_service.rb
@@ -0,0 +1,34 @@
+class SoundcloudService
+
+  def client
+    @client = Soundcloud.new(:client_id => ENV["soundcloud_client_id"],
+                            :client_secret => ENV["soundcloud_client_secret"],
+                            :redirect_uri => 'http://127.0.0.1:3000/auth/soundcloud/callback')
+  end
+
+  def user_hash(token)
+    parse(get_user(token))
+  end
+
+  def get_token(code)
+    client.exchange_token(code: code)["access_token"]
+  end
+
+  private
+
+  def parse(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def get_user(token)
+    connection.get("/me?oauth_token=#{token}")
+  end
+
+  def connection
+    Faraday.new(:url => 'https://api.soundcloud.com') do |faraday|
+      faraday.request  :url_encoded
+      faraday.response :logger
+      faraday.adapter  Faraday.default_adapter
+    end
+  end
+end
